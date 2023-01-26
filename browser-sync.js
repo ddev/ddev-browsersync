17303396ffd379cdbbf421e2e666700f89ec274d
Manuel Munz <munz@comuno.net>
2023-01-26 18:38:44 +0100

    Support multi-domain setups and create a proxy middleware for each domain

--- browser-sync.js
+++ browser-sync.js
@@ -1,21 +1,38 @@
 // #ddev-generated
-let docroot = process.env.DDEV_DOCROOT;
-let filesdir = process.env.DDEV_FILES_DIR;
-let url = process.env.DDEV_HOSTNAME;
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
-if (filesdir === "") {
-    filesdir = null
-}
+let docroot = process.env.DDEV_DOCROOT;
+let filesdir = process.env.DDEV_FILES_DIR ? process.env.DDEV_FILES_DIR : null;
+// process.env.DDEV_HOSTNAME contains a comma seperated list of all hostnames of the web container
+let env_hostnames = process.env.DDEV_HOSTNAME;
+let hostnames = env_hostnames.split(',')
+let url = hostnames[0];
+let browsersyncPort = 3000;
+let proxies = [];
+
+// Create proxies for each domain to use in browsersync middlewares
+hostnames.forEach(function (hostname) {
+    proxies.push({
+        'hostname': hostname,
+        'middleware': createProxyMiddleware('*', {target: 'https://' + hostname, changeOrigin: true})
+    })
+})
 
 module.exports = {
-
     files: [docroot, "app", "resources/views/**/*.php"],
     ignore: ["node_modules", filesdir, "vendor"],
     open: false,
     ui: false,
     server: false,
+    middleware: function (req, res, next) {
+        hostnames.forEach(function (hostname, index) {
+            if (req.headers.host === hostname + ':' + browsersyncPort) {
+                proxies[index].middleware(req, res, next);
+            }
+        })
+    },
     proxy: {
-        target: "localhost"
+        target: url,
     },
     host: url,
 }
