7288762d97f64d770b90e29dbb68c7bd9410bfc3
Jake Paris <jparis@bates.edu>
2025-04-15 10:47:12 -0400

    Update the text of the comment which is scanned/used to place new config

--- scripts/setup-wordpress-settings.sh
+++ scripts/setup-wordpress-settings.sh
@@ -23,18 +23,17 @@ if grep -q "/\*\* Include for ddev-browsersync to modify WP_HOME and WP_SITEURL.
 fi
 
 echo "Adding wp-config-ddev-browsersync.php to: ${SETTINGS_FILE_NAME}"
-
+ 
 # Append our code before the ddev config comment.
 awk '
-/\/\/ Include for ddev-managed settings in wp-config-ddev.php./ {
+/\/\/ Include for settings managed by ddev./ {
     print "/** Include for ddev-browsersync to modify WP_HOME and WP_SITEURL. */"
     print "$ddev_browsersync_settings = dirname( __FILE__ ) . \"/wp-config-ddev-browsersync.php\";"
-    print ""
     print "if ( is_readable( $ddev_browsersync_settings ) ) {"
     print "    require_once( $ddev_browsersync_settings );"
     print "}"
     print ""
-    print "// Include for ddev-managed settings in wp-config-ddev.php."
+    print "// Include for settings managed by ddev."
     next
 }
 {print}
